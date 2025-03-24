0d5970d022d2b2b28805d8d99e01c97f9cc23cb3
Vishal Palaniappan <vishal.palaniappan@yscope.com>
2025-03-24 13:13:05 -0400

    Add story book configuration with a sample app.

---
+++ src/components/Sample/Sample.jsx
@@ -0,0 +1,3 @@
+export const Sample = ({title}) => {
+    return <>{title}</>;
+}
---
+++ src/stories/Sample.stories.js
@@ -0,0 +1,11 @@
+import { Sample } from "../components/Sample";
+
+export default {
+    title: 'Example/Sample', 
+    component: Sample, 
+    args: { title: "default title"}
+};
+
+export const Variation1 = {
+    args: {title: "Variation1 title"}
+} 
